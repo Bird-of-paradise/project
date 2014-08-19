b50eaab162f46e9d36181e085fe0734383f9835f
mhl-yo <myjubox@gmail.com>
2014-08-19 10:38:49 +0300

    Adding the main content.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/style.css"/>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <h2>Still under construction</h2>
+</body>
+</html>`
