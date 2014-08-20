3a780acc65f9eb75383d7b1871716624373139b7
mhl-yo <myjubox@gmail.com>
2014-08-21 00:26:55 +0300

    Changing the home page

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 
-<?php require_once("php/header.php");?>
+<?php require_once("php/testHeader.php");?>
 
 <div class="main">
+
+    <div class="indexLogo">
+        <img src="image/Logo.png" alt="Logo"/>
+        <div class="login">
+            <?php require_once("loginForm.php");?>
+        </div>
+    </div>
+
 </div>
 
 <?php require_once("php/footer.php");?>
