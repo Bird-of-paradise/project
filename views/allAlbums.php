c56bad8f0aef20f049bb52f7ce7ee4df4e7a280d
mhl-yo <myjubox@gmail.com>
2014-08-24 15:50:39 +0300

    allAlbums

--- views/allAlbums.php
+++ views/allAlbums.php
@@ -1,8 +1,11 @@
-<div style="text-align: center">
-    I am views/allAlbum.php.  I will show  albums.
-</div>
+<link rel="stylesheet" href="../styles/table.css"/>
+<?php
+require_once '../models/allAlbums.php';
+?>
 
 
-
-
-
+<?php
+while($info = mysql_fetch_array( $data )) {
+    Print "<div class='albums'><h3>".$info['name'] . "</h3>". "<img src=".$info['img_file']." alt=".$info['name']."/>"."</div>";
+}
+?>
