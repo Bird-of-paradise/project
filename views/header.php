2cb04fdb10b577f629e722a31e7d2aacc87adefe
nikydobrev <nikydobrev@gmail.com>
2014-08-26 14:28:44 +0300

    openAlbum - showSong issue

--- views/header.php
+++ views/header.php
@@ -3,6 +3,10 @@
 <head lang="en">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles/style.css"/>
+    <link rel="stylesheet" type="text/css" href="styles/inlineplayer.css" />
+    <link rel="stylesheet" type="text/css" href="styles/flashblock.css" />
+    <script type="text/javascript" src="js/soundmanager2.js"></script>
+    <script type="text/javascript" src="js/inlineplayer.js"></script>
     <title>Welcome</title>
 </head>
 <body>
--- views/showSongs.php
+++ views/showSongs.php
@@ -1,25 +1,12 @@
-<table border="1">
+<ul class="graphic">
 <?php for ($i = 0; $i < count($rows); $i++) :
 ?>
 <tr>
-    <td><span id="dummy"></span>
-        <a href="#" ><img src="images/play_button.jpg" width="10" height="20" border="0" onclick="playSound('<?php echo($rows[$i]['file_name'])?>');" /></a>
-    </td>
-    <td><?= $rows[$i]['id']?></td>
-    <td><?= $rows[$i]['name']?></td>
-    <td><?= $rows[$i]['artist']?></td>
-    <td><?= $rows[$i]['genre']?></td>
-    <td><a href=<?="?action_user=5&id_song=" . $rows[$i]['id']?>>edit</a> / <a href=<?="?action_user=6&id_song=" . $rows[$i]['id']?>>remove</a></td>
-</tr>
+    <li><a href="music\<?php echo($rows[$i]['file_name'])?>"><?php echo($rows[$i]['name'])?></a></li>
 <?php endfor; ?>
-</table>
+</ul>
 
-<script language="javascript" type="text/javascript">
-    function playSound(soundfile) {
-        document.getElementById("dummy").innerHTML=
-            "<embed src=\"music\\"+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
-    }
-</script>
+
 
 
 
