cc825feceb5d17ccd793d422ff43b80f648ed29b
TzvetanIG <cecoig@mail.bg>
2014-08-26 21:49:17 +0300

    fix bug with alert

--- userPanel.php
+++ userPanel.php
@@ -3,7 +3,8 @@ session_start();
 require_once("controlers/controler.php");
 require_once("views/header.php");
 if(isset($_SESSION['alert'])){
-    echo$_SESSION['alert'];
+    echo $_SESSION['alert'];
+    unset($_SESSION['alert']);
 }
 require_once($view);
 require_once("views/footer.php");
